7e94442c7c1db990442af587363644a6d0e9865c
Henry Park <codedenma@gmail.com>
2022-12-09 15:13:49 -0800

    initial release

---
+++ demo.py
@@ -0,0 +1,21 @@
+from typing import Optional, List
+import collections
+import itertools
+import functools
+import math
+import string
+import random
+import bisect
+import re
+import operator
+import heapq
+import queue
+from queue import PriorityQueue
+from itertools import combinations, permutations
+from functools import lru_cache
+from collections import defaultdict, OrderedDict, deque, Counter
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pass
