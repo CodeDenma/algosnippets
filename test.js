de14e8bea0d75c6fb3450adcbfddc90ab4ba7ae5
Henry Park <codedenma@gmail.com>
2022-12-22 23:48:59 -0800

    js beta

--- test.js
+++ test.js
@@ -1,16 +1,3 @@
-const lodash = require('lodash');
-const util = require('util');
-
-
-// console.log(PriorityQueue)
-// console.log(Queue);
-
-// console.log(global);
-// console.log(ListNode);
-// console.log(isArray)
-
-// console.log(util);
-
-console.log(lodash)
-
-
+const _ = require('lodash');
+const { PriorityQueue, MinPriorityQueue, MaxPriorityQueue } = require('@datastructures-js/priority-queue');
+const { Queue } = require('@datastructures-js/queue');
